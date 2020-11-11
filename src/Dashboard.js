ae43e1ed92145c3c5a8227670ea9892d3b18bc5a
Brian Bachow <bmbachow@gmail.com>
2020-11-10 18:30:07 -0800

    need state

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -5,15 +5,12 @@ import {Link} from "react-router-dom";
 
 export class Dashboard extends Component {
 
+toggle = () => {
+    
+}
 
 render(){
 
-    let icon = this.icons['down'];
-
-    if(this.state.expanded){
-        icon = this.icons['up'];   
-    }
-    
     return (
         <section className="flex-container">
             <banner>
@@ -26,22 +23,22 @@ render(){
                 <h3>Categories</h3>
                 <div>
                     <span>Health</span>
-                        <button onClick={() => this.toggle()}>{icon}</button>
+                        <button onClick={() => this.toggle()}>+</button>
                         <Link to='/edit/:category' className="link">Edit Category</Link>
                             <HabitDropDown/>
                     <div className="habit">
-                        <button>Create Habit</button>
+                    <div className="create"><Link to='/add/habit' id="create-habit-button"><button>Create Habit</button></Link></div>
                     </div>
                 </div>
                 <div>
                     <span>Productivity</span>
-                    <button onClick={() => this.toggle()}>{icon}</button>
+                    <button onClick={() => this.toggle()}>+</button>
                 </div>
                 <div>
                     <span>Emotional Regulation</span>
-                    <button onClick={() => this.toggle()}>{icon}</button>
+                    <button onClick={() => this.toggle()}>+</button>
                 </div>
-                <div className="create"><Link to='/add/category' id="create-category-button">Create Category</Link></div>
+                <div className="create"><Link to='/add/category' id="create-category-button"><button>Create Category</button></Link></div>
             </section>
 
         </section>
