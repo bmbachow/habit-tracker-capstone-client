d68ea1c079008e87c7120b05ed24954fbf934e26
Brian Bachow <bmbachow@gmail.com>
2020-11-27 20:44:38 -0800

    changed api endpoint

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
+  // API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://habit-tracker-capstone.herokuapp.com/api',
   TOKEN_KEY: 'habit-tracker-auth-token',
 };
